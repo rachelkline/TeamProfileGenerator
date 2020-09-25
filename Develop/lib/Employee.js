913b191d1f1a0fbbc2365ca81a26b4bff7642270
Rachel Kline <rach3lkline@gmail.com>
2020-09-25 15:42:09 -0400

    Script for Employee Cards

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Employee class
+//set up shared properties between engineer, manager, and intern
+
+class Employee{
+    constructor(name, email, id){
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+    getName(){
+        return this.name;
+    }
+    getId(){
+        return this.id;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getRole(){
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
+
