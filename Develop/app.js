3f1fa608676ba100afa7cb701ba4ca86bb93c580
Rachel Kline <rach3lkline@gmail.com>
2020-09-29 17:54:14 -0400

    ReadMe and cleaned up code

--- Develop/app.js
+++ Develop/app.js
@@ -14,97 +14,106 @@ const render = require("./lib/htmlRenderer");
 
 let employees = [];
 
-async function mainHTML() {
-  // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
+promptUserManager();
+async function promptUserManager() {
 
-        await promptUser()
-        var renderEmp = render(employees);
+    response = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your team manager's name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Enter your team manager's employee ID.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter your team manager's email address.",
+            name: "email"
 
-   // After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
-    fs.writeFile(outputPath, renderEmp, function(err)
-   { if(err) throw err;
-       return console.log(err);
-   })
+        },
+        {
+            type: "input",
+            message: "Enter your team manager's office number.",
+            name: "officeNumber"
+        }
+    ]).then((response) => {
+        const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+        employees.push(manager);
+        addTeamMembers();
+    })
+}
+function addTeamMembers() {
+    responseNext = inquirer.prompt([
+        {
+            name: "newEmployee",
+            message: "Do you want to add another employee?",
+            type: "list",
+            choices: ["Yes", "No"]
+        }
+    ]).then(function (responseNext) {
+        if (responseNext.newEmployee === "Yes") {
+            newEmployee();
+        } else if (responseNext.newEmployee === "No") {
+            console.log("Generating Team...")
+            console.log("Team Generated!");
+            mainHTML();
+        }
+    });
+}
 
-};
-
-// Write code to use inquirer to gather information about the development team members, and to create objects for each team member (using the correct classes as blueprints!)
-
-async function promptUser() {
+async function newEmployee() {
     let responseDone = "";
     do {
-        try {
-            response = await inquirer.prompt([
+        response = await inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter team member's name.",
+                name: "name"
+            },
+            {
+                type: "list",
+                message: "Select the team member's role.",
+                choices: ["Engineer", "Intern"],
+                name: "role"
+            },
+            {
+                type: "input",
+                message: "Enter the team member's employee ID.",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Enter the team member's email address.",
+                name: "email"
+            }
+        ]);
+        const employee = new Employee(response.name, response.id, response.email);
+        let response2 = ""
+        if (response.role === "Engineer") {
+            response2 = await inquirer.prompt([
                 {
+                    name: "github",
                     type: "input",
-                    message: "Enter team member's name.",
-                    name: "name"
-                },
-                {
-                    type: "list",
-                    message: "Select the team member's role.",
-                    choices: ["Engineer", "Manager", "Intern"],
-                    name: "role"
-                },
-                {
-                    type: "input",
-                    message: "Enter the team member's employee ID.",
-                    name: "id"
-                },
-                {
-                    type: "input",
-                    message: "Enter the team member's email address.",
-                    name: "email"
+                    message: "What is the engineer's github username?"
                 }
             ]);
-
-            const employee = new Employee(response.name, response.id, response.email);
-            let response2 = ""
-            if (response.role === "Engineer") {
-                response2 = await inquirer.prompt([
-                    {
-                        name: "github",
-                        type: "input",
-                        message: "What is your github username?"
-                    }
-                ]);
-                const engineer = new Engineer(response.name, response.id, response.email, response2.github);
-
-                console.log(engineer);
-                employees.push(engineer);
-
-            } else if (response.role === "Intern") {
-                response2 = await inquirer.prompt([
-                    {
-                        name: "school",
-                        type: "input",
-                        message: "What college or university do you attend?"
-                    }
-                ]);
-                const intern = new Intern(response.name, response.id, response.email, response2.school);
-
-                console.log(intern)
-                employees.push(intern);
-
-
-            } else if (response.role === "Manager") {
-                response2 = await inquirer.prompt([
-                    {
-                        name: "officeNumber",
-                        type: "input",
-                        message: "What is your office phone number?"
-                    }
-                ]);
-                const manager = new Manager(response.name, response.id, response.email, response2.officeNumber);
-
-                console.log(manager);
-                employees.push(manager);
-            }
-        } catch (err) {
-            return console.log(err);
+            const engineer = new Engineer(response.name, response.id, response.email, response2.github);
+            employees.push(engineer);
+        } else if (response.role === "Intern") {
+            response2 = await inquirer.prompt([
+                {
+                    name: "school",
+                    type: "input",
+                    message: "What college or university does the intern attend?"
+                }
+            ]);
+            const intern = new Intern(response.name, response.id, response.email, response2.school);
+            employees.push(intern);
         }
-
-        console.log(employees);
+        
         responseDone = await inquirer.prompt([
             {
                 name: "continue",
@@ -114,10 +123,22 @@ async function promptUser() {
             }
         ]);
     } while (responseDone.continue === "Yes");
-    if(responseDone.continue === "No"){
+    if (responseDone.continue === "No") {
         console.log("Generating Team...")
+        console.log("Team Generated!");
+        mainHTML();
     }
-    
-
 }
-mainHTML();
+// Write code to use inquirer to gather information about the development team members, and to create objects for each team member (using the correct classes as blueprints!)
+async function mainHTML() {
+    // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
+    var renderEmp = render(employees);
+
+    // After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
+    fs.writeFile(outputPath, renderEmp, function (err) {
+        if (err)
+            throw err;
+
+    })
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # TeamProfileGenerator
+
+![Language](https://img.shields.io/static/v1?label=JavaScript&message=language&color=brightgreen)
+
+## Description
+
+  *The what, why, and how:*
+
+  In this assignment, we were tasked with building a software engineering team generator comand line application using the Inquirer package. The user can input any number of team members, and they may be a mix of engineers and interns. The assignment must also pass all unit tests. When the user has completed building the team, the application will then create an HTML file that displays a nicely formatted team roster based on the user-provided information.
+
+  The employees are generated using a standard "Employee" class, then based on the user-input, they are then classified as either a Manager, Engineer, or Intern. Depending on the job title chosen, different questions will be prompted and appended to the HTML file.
+  
+  We were given free-reign for styling. In my example, I used my favorite video game characters (from the Legend of Zelda) and decided to build my color-schemes and fonts around that game!
+
+  ---
+
+## Video Tutorial
+
+Click the play button to be redirected to the video tutorial.
+
+[![IMAGE](Develop/assets/video.png)](https://drive.google.com/file/d/1j9GrG6QTUkGiJMAumKA4bWCoDt3iasW_/preview) 
+
+  ---
+## Table of Contents
+
+
+  - [Description](#description)
+  - [Video Tutorial](#video-tutorial)
+  - [Usage](#usage)
+  - [Questions](#questions)
+ 
+ ---
+
+<img src = "laptop-house-solid.svg" width="40">
+
+
+## Usage
+  *Instructions and examples for use:*
+
+Download the inquirer package in order to run this application using npm install. Then, run the tests using npm run test to ensure that they all pass. This is crucial to the execution of the final HTML file.
+  
+---
+
+<img src = "question-circle-regular.svg" width="40">
+
+## Questions?
+
+  *For any questions about this application, please contact me via the information below:*
+
+  GitHub: https://github.com/users/rachelkline
+  
+  Email: rachel_kline@icloud.com
+
+---
+  <br>
+
+  [Icons provided by https://fontawesome.com/]
